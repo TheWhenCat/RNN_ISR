dc5da9b59869ec11dd14c6318dd97fa355439e43
Dhruva Venkat <dhruva735@gmail.com>
2019-02-06 00:21:18 -0500

    Finished Neural Net Basic
    Needs to be rectified in the onehotencoder area tomorrow

--- DataPreProcessing/Template.py
+++ DataPreProcessing/Template.py
@@ -9,28 +9,27 @@ import pandas as pd
 dataset = pd.read_csv("Data.csv")
 X = dataset.iloc[:, 0:-1].values
 Y = dataset.iloc[:, -1].values
-print(Y)
-
 
 #Dealing with Missing Data
-from sklearn.preprocessing import Imputer
-imputer = Imputer(missing_values = "NaN", strategy = "mean", axis = 0)
-imputer.fit(X[0:, 3:4])
-X[0:, 3:4] = imputer.transform(X[0:, 3:4])
-imputer.fit(X[0:, 7:13])
-X[0:, 7:13] = imputer.transform(X[0:, 7:13])
+#from sklearn.preprocessing import Imputer
+#imputer = Imputer(missing_values = "NaN", strategy = "mean", axis = 0)
+#imputer.fit(X[0:, 3:4])
+#X[0:, 3:4] = imputer.transform(X[0:, 3:4])
+#imputer.fit(X[0:, 7:13])
+#X[0:, 7:13] = imputer.transform(X[0:, 7:13])
 
 
 #Encodcing Categorical Data
 #Not working come back to this
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_X = LabelEncoder()
-X = X[0:, :]
-X[0:, 2] = labelencoder_X.fit_transform(X[0:, 2])
-X[0:, 4] = labelencoder_X.fit_transform(X[0:, 4])
-X[0:, 5] = labelencoder_X.fit_transform(X[0:, 5])
-onehotencoder = OneHotEncoder(categorical_features = [2,4,5])
-X = onehotencoder.fit_transform(X).toarray()
+X[:, 1] = labelencoder_X.fit_transform(X[:, 1])
+X[:, 2] = labelencoder_X.fit_transform(X[:, 2])
+X[:, 4] = labelencoder_X.fit_transform(X[:, 4])
+X[:, 5]= labelencoder_X.fit_transform(X[:, 5])
+#onehotencoder = OneHotEncoder(categorical_features = [1,2])
+#X = onehotencoder.fit_transform(X).toarray()
+X = X[:, 2:]
 
 labelencoder_Y = LabelEncoder()
 Y = labelencoder_Y.fit_transform(Y)
@@ -70,4 +69,4 @@ classifier.add(Dense(output_dim=1, init="uniform", activation = "sigmoid"))
 classifier.compile(optimizer="adam", loss="binary_crossentropy",metrics=["accuracy"])
 
 #Fitting ANN to Training Set
-classifier.fit(X_train, Y_train, batch_size=10, epochs=200)
+classifier.fit(X_train, Y_train, batch_size=10, nb_epoch=100)
